667c43c6dc6ec037d64b9afc5bda9d4e7076625d
nesaranaeem <hello@nesaran.com>
2022-12-27 16:15:14 +0600

    medicine company name in recent cards added

--- components/cards/DoctorCard.js
+++ components/cards/DoctorCard.js
@@ -13,19 +13,26 @@ const DoctorCard = ({ doctor }) => {
   } = doctor;
   const [doctorSpecialty, setDoctorSpecialty] = useState([]);
   const [doctorOrganization, setDoctorOrganization] = useState([]);
+  const [loading, setloading] = useState(true);
   useEffect(() => {
     fetch(
       `https://medicinebd-medicine-details-server.vercel.app/v1/doctor-speciality/${specialty}`
     )
       .then((res) => res.json())
-      .then((data) => setDoctorSpecialty(data.result));
+      .then((data) => {
+        setDoctorSpecialty(data.result);
+        setloading(false);
+      });
   }, [doctorSpecialty]);
   useEffect(() => {
     fetch(
       `https://medicinebd-medicine-details-server.vercel.app/v1/doctor-organization/${organization}`
     )
       .then((res) => res.json())
-      .then((data) => setDoctorOrganization(data.result));
+      .then((data) => {
+        setDoctorOrganization(data.result);
+        setloading(false);
+      });
   }, [doctorOrganization]);
 
   return (
@@ -39,73 +46,85 @@ const DoctorCard = ({ doctor }) => {
         <p>{qualification}</p>
 
         <div>
-          <div className="my-3 grid gap-4 grid-cols-1 justify-items-center">
-            {doctorOrganization.map((organizationName) => (
-              <Link href={`/hospital/${organization}`}>
-                <>
-                  <div className="badge badge-lg">
-                    <FaHospitalUser />
-                    {organizationName.name.length > 30 ? (
-                      <div
-                        className="tooltip"
-                        data-tip={`${organizationName.name}`}
-                      >
-                        <button className="badge badge-lg">
-                          {organizationName.name.slice(0, 30)}...
-                        </button>
+          {loading ? (
+            <progress className="progress w-56"></progress>
+          ) : (
+            <>
+              <div className="my-3 grid gap-4 grid-cols-1 justify-items-center">
+                {doctorOrganization.map((organizationName) => (
+                  <Link href={`/hospital/${organization}`}>
+                    <>
+                      <div className="badge badge-lg">
+                        <FaHospitalUser />
+                        {organizationName.name.length > 30 ? (
+                          <div
+                            className="tooltip"
+                            data-tip={`${organizationName.name}`}
+                          >
+                            <button className="badge badge-lg">
+                              {organizationName.name.slice(0, 30)}...
+                            </button>
+                          </div>
+                        ) : (
+                          <>{organizationName.name}</>
+                        )}
                       </div>
-                    ) : (
-                      <>{organizationName.name}</>
-                    )}
-                  </div>
-                </>
-              </Link>
-            ))}
-          </div>
-          <div className="grid gap-4 grid-cols-1 justify-items-center">
-            {doctorSpecialty.map((specialtyName) => (
-              <>
-                <Link href={`/doctor-speciality/${specialty}`}>
+                    </>
+                  </Link>
+                ))}
+              </div>
+            </>
+          )}
+          {loading ? (
+            <progress className="progress w-56"></progress>
+          ) : (
+            <>
+              <div className="grid gap-4 grid-cols-1 justify-items-center">
+                {doctorSpecialty.map((specialtyName) => (
                   <>
-                    <div className="badge badge-lg">
-                      <FaHeartbeat />
-                      {specialtyName.name.length > 25 ? (
-                        <div
-                          className="tooltip"
-                          data-tip={`${specialtyName.name}`}
-                        >
-                          <button className="badge badge-lg">
-                            {specialtyName.name.slice(0, 25)}...
-                          </button>
+                    <Link href={`/doctor-speciality/${specialty}`}>
+                      <>
+                        <div className="badge badge-lg">
+                          <FaHeartbeat />
+                          {specialtyName.name.length > 25 ? (
+                            <div
+                              className="tooltip"
+                              data-tip={`${specialtyName.name}`}
+                            >
+                              <button className="badge badge-lg">
+                                {specialtyName.name.slice(0, 25)}...
+                              </button>
+                            </div>
+                          ) : (
+                            specialtyName.name
+                          )}
                         </div>
-                      ) : (
-                        specialtyName.name
-                      )}
-                    </div>
-                  </>
-                </Link>
-                <Link href={`/doctor-speciality/${specialty}`}>
-                  <>
-                    <div className="badge badge-lg">
-                      <FaHeartbeat />
-                      {specialtyName.bangla_name.length > 25 ? (
-                        <div
-                          className="tooltip"
-                          data-tip={`${specialtyName.bangla_name}`}
-                        >
-                          <button className="badge badge-lg">
-                            {specialtyName.bangla_name.slice(0, 25)}...
-                          </button>
+                      </>
+                    </Link>
+                    <Link href={`/doctor-speciality/${specialty}`}>
+                      <>
+                        <div className="badge badge-lg">
+                          <FaHeartbeat />
+                          {specialtyName.bangla_name.length > 25 ? (
+                            <div
+                              className="tooltip"
+                              data-tip={`${specialtyName.bangla_name}`}
+                            >
+                              <button className="badge badge-lg">
+                                {specialtyName.bangla_name.slice(0, 25)}...
+                              </button>
+                            </div>
+                          ) : (
+                            specialtyName.bangla_name
+                          )}
                         </div>
-                      ) : (
-                        specialtyName.bangla_name
-                      )}
-                    </div>
+                      </>
+                    </Link>
                   </>
-                </Link>
-              </>
-            ))}
-          </div>
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
--- components/home/recent/RecentMedicineCard.js
+++ components/home/recent/RecentMedicineCard.js
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+
 const RecentMedicineCard = ({ medicine }) => {
-  const { brand_name, form, packsize, strength, price } = medicine;
+  const { brand_name, form, packsize, strength, price, company_id } = medicine;
+  const [company, setCompany] = useState([]);
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      `https://medicinebd-medicine-details-server.vercel.app//v1/medicine-company/${company_id}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setCompany(data.result);
+        setloading(false);
+      });
+  }, [company_id]);
   return (
     <div className="card w-11/12 h-auto lg:w-96 xl:w-96 bg-neutral-content shadow-xl">
       <div className="card-body">
@@ -7,16 +22,27 @@ const RecentMedicineCard = ({ medicine }) => {
           <h2 className="card-title">{brand_name}</h2>
           <h4 className="mx-3 badge badge-lg">{form}</h4>
         </div>
+        {loading ? (
+          <progress className="progress w-56"></progress>
+        ) : (
+          <>
+            {company.map((singleCompany) => (
+              <>
+                <p className=" rounded-full bg-amber-100 px-2.5 py-0.5 text-amber-700 whitespace-nowrap text-sm text-center">
+                  {singleCompany.company_name}
+                </p>
+              </>
+            ))}
+          </>
+        )}
         <div className="grid gap-2 grid-cols-1 md:grid-cols-3 lg:grid-cols-3 justify-items-center">
           {packsize === null ? (
             ""
           ) : (
-            <div className="badge badge-secondary py-3">{packsize}</div>
+            <div className="badge badge-secondary">{packsize}</div>
           )}
-          <div className="badge py-3">{strength}</div>
-          <div className="badge badge-primary py-3">
-            {Math.round(price)} BDT/Unit
-          </div>
+          <div className="badge">{strength}</div>
+          <div className="badge badge-primary">{Math.round(price)} TK/Unit</div>
         </div>
         <div className="card-actions justify-end">
           <button className="btn btn-primary">Details</button>
