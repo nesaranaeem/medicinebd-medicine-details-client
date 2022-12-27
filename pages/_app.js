62a466b58ff727d8b6d494328733101b42ae5f78
nesaranaeem <hello@nesaran.com>
2022-12-28 04:11:41 +0600

    theme updated

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,15 @@ import SEO from "../next-seo.config";
 import Main from "../components/layout/Main";
 import NextNProgress from "nextjs-progressbar";
 import "../styles/globals.css";
-import { ThemeProvider } from "next-themes";
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       <NextNProgress />
       <Main>
         <DefaultSeo {...SEO} />
-        <ThemeProvider attribute="data-theme">
-          <Component {...pageProps} />
-        </ThemeProvider>
+
+        <Component {...pageProps} />
       </Main>
     </>
   );
